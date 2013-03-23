e439d3889c7c787c976c3c9f58e49b81acbc6f93
Vijay Thurimella <vt@cmu.edu>
2013-03-23 02:15:57 -0400

    Added bonus functionality

--- yahtzee.sh
+++ yahtzee.sh
@@ -1,10 +1,29 @@
 #!/bin/bash
+# Yahtzee is a terminal version of the game yahtzee.
+#     Copyright (C) 2013 Vijay Thurimella
+
+#     This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+
+#     You should have received a copy of the GNU General Public License
+#     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 declare -a board
 declare -a section 
 declare store
 declare score
 declare pick
 declare name
+declare lower_total
+declare got_yahtzee
+declare YNUM
 
 init(){
   section[1]="Ones "
@@ -22,6 +41,9 @@ init(){
   section[12]="Yahtzee "
   section[13]="Chance "
   score=0
+  lower_total=0
+  has_yahtzee=""
+  YNUM=12
 }
 
 update_section(){
@@ -56,15 +78,23 @@ pick_section(){
     pick_section $s
     return $?
   fi
-  if echo ${section[$1]} | grep "X" >> /dev/null; then
+  if is_used "${section[$1]}"; then
    echo "$(echo ${section[$1]} | cut -dX -f1)already picked"
    return 0
   else
     temp=$(compute_score $1)
     score=$(($score + $temp))
+    if [[ 1 -le $1 && $1 -le 6 ]]; then
+      lower_score=$(($lower_score + $temp))
+    fi
     section[$1]="$(echo ${section[$1]} | cut -d "-" -f1) X"
     return 1
   fi
+}
+
+is_used(){
+  echo $1 | grep "X" >> /dev/null 
+  return $?
 }
 
 print_used(){
@@ -72,7 +102,7 @@ print_used(){
   local i
   used="Used: "
   for ((i=1; i<=13; i++)); do
-    if echo ${section[$i]} | grep "X" >> /dev/null; then
+    if is_used "${section[$i]}"; then
       used="$used$(echo ${section[$i]} | cut -d X -f1 | tr -d " "), "
     fi
   done
@@ -104,11 +134,11 @@ has_shortstraight(){
   local len1
   local len2
   local len3
-  len1=$(comm -12 <(echo "1 2 3 4" | xargs -n1) \
+  len1=$(comm -12 <(echo {1..4} | xargs -n1) \
           <(echo ${board[@]} | xargs -n1 | sort -n) | wc -l)
-  len2=$(comm -12 <(echo "2 3 4 5" | xargs -n1) \
+  len2=$(comm -12 <(echo {2..5} | xargs -n1) \
           <(echo ${board[@]} | xargs -n1 | sort -n) | wc -l)
-  len3=$(comm -12 <(echo "3 4 5 6" | xargs -n1) \
+  len3=$(comm -12 <(echo {3..6} | xargs -n1) \
           <(echo ${board[@]} | xargs -n1 | sort -n) | wc -l)
   if [[ $len1 -eq 4 || $len2 -eq 4 || $len3 -eq 4 ]]; then
     return 0
@@ -119,9 +149,9 @@ has_shortstraight(){
 has_largestraight(){
   local len1
   local len2
-  len1=$(comm -12 <(echo "1 2 3 4 5" | xargs -n1) \
+  len1=$(comm -12 <(echo {1..5} | xargs -n1) \
           <(echo ${board[@]} | xargs -n1 | sort -n) | wc -l)
-  len2=$(comm -12 <(echo "2 3 4 5 6" | xargs -n1) \
+  len2=$(comm -12 <(echo {2..6} | xargs -n1) \
           <(echo ${board[@]} | xargs -n1 | sort -n) | wc -l)
   if [[ $len1 -eq 5 || $len2 -eq 5 ]]; then
     return 0
@@ -140,15 +170,16 @@ has_fullhouse(){
 }
 
 compute_score(){
+  num=${board[0]}
   case $1 in
     [1-6]) echo $(($1 * $(count_numbers $1)));;
-    7) if has_n 3 || has_n 4; then
+    7) if has_n 3 || has_n 4 || has_n 5; then
          echo $(sum_board)
        else
-         echo 0
+          echo 0
        fi
        ;;
-    8) if has_n 4; then
+    8) if has_n 4 || has_n 5; then
          echo $(sum_board)
        else
          echo 0
@@ -157,23 +188,39 @@ compute_score(){
     9) if has_fullhouse; then
          echo 25
        else 
-         echo 0
+          if is_used "${section[YNUM]}" && has_n 5 && is_used "${section[num]}";
+          then
+            echo 25
+          else
+            echo 0
+          fi 
        fi
        ;;
     10) if has_shortstraight; then
           echo 30
         else
-          echo 0
+          if is_used "${section[YNUM]}" && has_n 5 && is_used "${section[num]}";
+          then
+            echo 30
+          else
+            echo 0
+          fi 
         fi
         ;;
     11) if has_largestraight; then
           echo 40
         else
-          echo 0
+          if is_used "${section[YNUM]}" && has_n 5 && is_used "${section[num]}";
+          then
+            echo 30
+          else
+            echo 0
+          fi 
         fi
         ;;
     12) if has_n 5; then
          echo 50
+         got_yahtzee=1
        else
          echo 0
        fi
@@ -205,7 +252,7 @@ difference(){
 roll(){
   local i
   for i in $(echo $store); do
-    board[$i]=$(($(($RANDOM % 6)) + 1))
+    board[$i]=$(echo "($RANDOM % 6) + 1" | bc)
   done
 }
 
@@ -217,8 +264,8 @@ for ((i=1; i<=13; i++)); do
       echo "Dice   ------> " ${board[@]}
       echo "Hold Labels -> " {1..5}
     if [ $j -ne 2 ]; then
-      read -p "Hold which dies (\"skip\" to score current): " store
-      if [[ -n $store &&  $store = "skip" ]]; then
+      read -p "Hold which dies (\"skip\" to score): " store
+      if [[ -n $store && $store = "skip" ]]; then
         store=$(echo {1..5})
         j=1
       else
@@ -228,6 +275,10 @@ for ((i=1; i<=13; i++)); do
    else
       clear
       echo "Dice   ------> " ${board[@]}
+      if [[ -n got_yahtzee && "has_n 5" ]]; then
+        echo "Yahtzee Bonus"
+        score=$(($score + 100))
+      fi
       update_section
       print_section
       read -p "Pick which Section: " pick
@@ -241,14 +292,20 @@ for ((i=1; i<=13; i++)); do
   done
 done
 
+clear
+if [[ 63 -le $lower_score ]]; then
+  echo "Received upper bonus!"
+  score=$(($score + 35))
+fi
+
 read -p "Enter first name: " name
 while [ -z "$name" ]; do
   read -p "Enter first name: " name
 done
-echo "$name $score" >> .yatzee_scoreboard.txt
+echo "$name $score" >> .yahtzee_scoreboard.txt
 
-cat .yatzee_scoreboard.txt | sort -r -k2 -n | head -10 >> .yat_score_sorted
-mv .yat_score_sorted .yatzee_scoreboard.txt
+cat .yahtzee_scoreboard.txt | sort -r -k2 -n | head -10 >> .yaht_score_sorted
+mv .yaht_score_sorted .yahtzee_scoreboard.txt
 
 echo "Scoreboard"
-cat .yatzee_scoreboard.txt
+cat .yahtzee_scoreboard.txt
